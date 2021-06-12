b92a606dc51a821084f89fdb508705d54b254cd0
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-12 16:12:17 +0530

    Add posts dynamically

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from datetime import date
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
-        "author": "Maximilian",
+        "author": "Sai Sridhar",
         "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
@@ -23,8 +23,8 @@ posts = [
     },
     {
         "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
+        "image": "coding.png",
+        "author": "Sai Sridhar",
         "date": date(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
@@ -43,7 +43,7 @@ posts = [
     {
         "slug": "into-the-woods",
         "image": "woods.jpg",
-        "author": "Maximilian",
+        "author": "Sai Sridhar",
         "date": date(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
@@ -62,11 +62,19 @@ posts = [
 ]
 
 
+def get_date(post):
+    return post['date']
+
 # Create your views here.
 
 
 def starting_page(request):
-    return render(request, "blog/starting-page.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+
+    return render(request, "blog/starting-page.html", {
+        "posts": latest_posts
+    })
 
 
 def posts(request):
