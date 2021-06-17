4d149273c143eeeba0052f91db7d7a2af946dd44
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-17 11:59:54 +0530

    Migrating to Class Based Views

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,35 @@
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 # Create your views here.
 
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
+class IndexView(ListView):
+    template_name = "blog/starting-page.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-    return render(request, "blog/starting-page.html", {
-        "posts": latest_posts
-    })
-
-
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
+class PostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
 
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
